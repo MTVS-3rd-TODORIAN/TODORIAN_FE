83a2d332a5148c8ee95ec74758abe658d7566846
daekwon2000 <daekklove@gmail.com>
2024-09-03 09:16:20 +0900

    [Feat] : inventory page 구현

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Main from './pages/Main';
 import Todo from './pages/todo/Todo';
+import Inventory from './pages/inventory/Inventory';
 // Import other components for routing here...
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <>
         <Route path="/main" element={<Main/>}/>
         <Route path="/today-deal" element={<Todo/>}/>
+        <Route path="/closet" element={<Inventory/>}/>
       </>
     </Routes>
   );
