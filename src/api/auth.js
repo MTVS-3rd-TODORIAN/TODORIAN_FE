5d14e4cafa0f2f7cecf4cd68314ac5fa94d8d0b0
INUK-ai <arheon2469@gmail.com>
2024-09-08 15:42:36 +0900

    Feat: Login

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const getToken = async (code, state) => {
+export const login = async (email, password) => {
   const baseURL = import.meta.env.VITE_BASE_URL;
 
   const res = await axios.post(`${baseURL}/auth/login`, {
-    code: code,
-    state: state,
+    email: email,
+    password: password,
   });
 
-  return res;
+  return res.data;
 };
--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -6,11 +6,19 @@ import ChickImg from '../../assets/images/mainPage/mainChick.png';
 
 const Login = () => {
   
-  const { search } = useLocation();
+  const { email, setEmail } = useState('');
+  const { password, setPassword } = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate('/main');  // '/main' 경로로 이동
+  const handleLogin = async () => {
+    try {
+      const res = await login(email, password);
+      console.log('Login successful : ' + res);
+
+      navigate('/main');  // '/main' 경로로 이동
+    } catch (err) {
+      console.error('Login failed :'+ err);
+    }
   };
 
   return (
@@ -18,8 +26,18 @@ const Login = () => {
       <Title>ToDorian</Title>
       <LoginForm>
         <ChickImage src={ChickImg} alt="Chick" />
-        <Input type="text" placeholder="아이디" />
-        <Input type="password" placeholder="비밀번호" />
+        <Input 
+          type="email" 
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+        <Input 
+          type="password" 
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+        />
         <LoginButton onClick={handleLogin}>로그인</LoginButton>
       </LoginForm>
     </LoginContainer>
