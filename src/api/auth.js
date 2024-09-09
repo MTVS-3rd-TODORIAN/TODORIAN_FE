ebdca76bc1ddd0b15ff2bffeb76581fa9b10b8ae
INUK-ai <arheon2469@gmail.com>
2024-09-09 17:32:50 +0900

    Feat: Login

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 
 export const login = async (email, password) => {
+
   const baseURL = import.meta.env.VITE_BASE_URL;
 
   const res = await axios.post(`${baseURL}/auth/login`, {
     email: email,
     password: password,
   });
+
+  const token = res.headers['Authorization'];
+
+  if(token && token.startsWith('Bearer ')) {
+    const jwtToken = token.split(' ')[1];
+    localStorage.setItem('token', jwtToken);
+
+    console.log('토큰 저장 성공:', jwtToken);
+  } else {
+    console.error('Authorization 헤더에 토큰이 없습니다.');
+  }
 
   return res.data;
 };
--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { login } from '../../api/auth';
 import styled from 'styled-components';
 import ChickImg from '../../assets/images/mainPage/mainChick.png';
