93fe781b88a8a09c39e06b6d3f198f3d92d73929
Virtus_Chae <ybd4731@gmail.com>
2024-10-14 15:34:52 +0900

    feat: member Entity 의 아이디를 받아 오는 로직 추가

--- src/api/member.js
+++ src/api/member.js
@@ -8,3 +8,13 @@ export const getMemberProfile = async () => {
       console.error('Failed to fetch member profile:', error);
     }
 }
+
+// 아이디 받는 로직 추가
+export const getMemberId = async () => {
+  try {
+    const response = await axiosInstance.get('/member/id');
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch member id:', error);
+  }
+}
